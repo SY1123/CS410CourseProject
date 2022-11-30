9dfc12b9097b08ec95b5f6d7fb095b6b22a4969c
chang <lichangaa@gmail.com>
2022-11-29 19:15:41 -0600

    flask run bm25 AND self_cosine

--- NLPSearch.py
+++ NLPSearch.py
@@ -4,44 +4,61 @@ import math
 import numpy as np
 from rank_bm25 import BM25Okapi
 
-df = pd.read_csv("songs_sentiment.csv", encoding='unicode_escape', sep=",")
-ll = df['lyrics'].to_list()
-cleanedList = [x for x in ll if x != 'NaN']
-cleanedList
-ll[1]
-len(df.columns)
+# df = pd.read_csv("songs_sentiment.csv", encoding='unicode_escape', sep=",")
+# ll = df['lyrics'].to_list()
+# cleanedList = [x for x in ll if x != 'NaN']
 # cleanedList
+# ll[1]
+# len(df.columns)
+# # cleanedList
 
-df_ = df[df.lyrics != 'Error: Could not find lyrics.']
-df_ = df_[df_['lyrics'].notna()]
-ll = df_['lyrics'].to_list()
+# df_ = df[df.lyrics != 'Error: Could not find lyrics.']
+# df_ = df_[df_['lyrics'].notna()]
+# ll = df_['lyrics'].to_list()
 
-# print(type(ll))
-#clean data to All_doc
-All_doc = ll
-delimi = ",.!?/&-:;@'..."
-All_bagofwords = []
-for i in range(0,len(All_doc)):
-    re.split("["+"\\".join(delimi)+"]", All_doc[i])
-    All_doc[i] = (' '.join(w for w in re.split(r"\W", All_doc[i]) if w)).lower()
-    # print(All_doc[i].split())
-    All_bagofwords.append(All_doc[i].split())
+# # print(type(ll))
+# # clean data to All_doc
+# All_doc = ll
+# delimi = ",.!?/&-:;@'..."
+# All_bagofwords = []
+# for i in range(0,len(All_doc)):
+#     re.split("["+"\\".join(delimi)+"]", All_doc[i])
+#     All_doc[i] = (' '.join(w for w in re.split(r"\W", All_doc[i]) if w)).lower()
+#     # print(All_doc[i].split())
+#     All_bagofwords.append(All_doc[i].split())
 # print(All_bagofwords[0])
 
 
-#Map word freq in each doc
-MAP = []
-for i in range(0,len(All_bagofwords)):
 
-    numWords = dict.fromkeys(All_bagofwords[i],0)
-    for word in All_bagofwords[i]:
-        numWords[word] +=1
-    # print("map",numWords)
-    MAP.append(numWords)
-# print(MAP[0])
+def fetchInMap(All_bagofwords):
+    MAP = []
+    for i in range(0,len(All_bagofwords)):
 
-allkey = set().union(*MAP)
-allkey
+        numWords = dict.fromkeys(All_bagofwords[i],0)
+        for word in All_bagofwords[i]:
+            numWords[word] +=1
+        # print("map",numWords)
+        MAP.append(numWords)
+    return(MAP)
+
+def mergeKey(MAP):
+    allkey = set().union(*MAP)
+    return(allkey)
+
+#==================================================================
+
+# #Map word freq in each doc
+# MAP = []
+# for i in range(0,len(All_bagofwords)):
+
+#     numWords = dict.fromkeys(All_bagofwords[i],0)
+#     for word in All_bagofwords[i]:
+#         numWords[word] +=1
+#     # print("map",numWords)
+#     MAP.append(numWords)
+# # print(MAP[0])
+
+# allkey = set().union(*MAP)
 
 
 #TF
@@ -53,21 +70,20 @@ def computeTF(wordDict, bagOfwords):
         tfDict[word] = count / float(bagOfwordsL)
     return tfDict
 
-TF = []
-for i in range(0,len(All_bagofwords)):
-    tf = computeTF(MAP[i],All_bagofwords[i])
-    TF.append(tf)
-    # print(tf)
-# print(TF[0])
+
+# TF = []
+# for i in range(0,len(All_bagofwords)):
+#     tf = computeTF(MAP[i],All_bagofwords[i])
+#     TF.append(tf)
 
 
 
 #IDF Inverse Data Frequency
-def computeIDF(documents):
+def computeIDF(documents,allkey):
     import math
     N = len(documents)
     
-    idfDict = dict.fromkeys(set().union(*MAP), 0)
+    idfDict = dict.fromkeys(allkey, 0)
     for document in documents:
         for word, val in document.items():
             if val > 0:
@@ -76,8 +92,8 @@ def computeIDF(documents):
     for word, val in idfDict.items():
         idfDict[word] = math.log(N / float(val))
     return idfDict
-idf = computeIDF(MAP)
-print(dict(list(idf.items())[0:20]))
+# idf = computeIDF(MAP)
+# print(dict(list(idf.items())[0:20]))
 
 
 def computeTFIDF(tfBagOfWords, idfs):
@@ -88,11 +104,10 @@ def computeTFIDF(tfBagOfWords, idfs):
 
 # print(idf['finished'] * TF[0])
 
-tfidf = []
-for i in range(0, len(TF)):
-    tfidf.append(computeTFIDF(TF[i],idf))
-# print(tfidf[0])
-# len(tfidf[0])
+# tfidf = []
+# for i in range(0, len(TF)):
+#     tfidf.append(computeTFIDF(TF[i],idf))
+
 
 
 #Start here is query proecessing
@@ -102,7 +117,7 @@ def query_BOW(input):
     input = ' '.join(w for w in re.split(r"\W", input) if w)
     return(input.split())
 
-def query_num(input):
+def query_num(input, allkey):
     result = dict.fromkeys(allkey, 0)
     for word in input:
         result[word] += 1
@@ -129,16 +144,16 @@ def query_computeIDF(documents):
         idfDict[word] = math.log(N / float(val))
     return idfDict
 
-query = "They're rotting my brain"
-query = query.lower()
-query_bow = query_BOW(query)
-query_fr = query_num(query_bow)
-query_tf = query_computeTF(query_fr,query_bow)
-len(query_tf)
+# query = "They're rotting my brain"
+# query = query.lower()
+# query_bow = query_BOW(query)
+# query_fr = query_num(query_bow)
+# query_tf = query_computeTF(query_fr,query_bow)
+# len(query_tf)
 
 
 import math
-def cosine_similarity(query, datas):
+def cosine_similarity(query, datas,query_tf):
     result = []
     for data in datas:
         numerator  = 0
@@ -151,12 +166,12 @@ def cosine_similarity(query, datas):
             denom_d += pow(data[key],2)
         result.append(numerator / (math.sqrt(denom_q) * math.sqrt(denom_d) ))
     return(result)
-query_rank = cosine_similarity(query_tf,tfidf)
+# query_rank = cosine_similarity(query_tf,tfidf)
 
 #this will give the index of top five rank selection for the query
 #same index can be used to get other data from ll variable
-import numpy as np
-np.argsort(query_rank)[-5:]
+# import numpy as np
+# np.argsort(query_rank)[-5:]
 
 
 
@@ -168,10 +183,12 @@ def bm25_index(All_doc, query):
 
     bm25 = BM25Okapi(tokenized_corpus)
     # query = "I m a little unsteady A little unsteady"
-    tokenized_query = query.split(" ")
-
+    tokenized_query = query_BOW(query)
     doc_scores = bm25.get_scores(tokenized_query)
+    print(doc_scores)
     # array([0.        , 0.93729472, 0.        ])
 
     #this will directly give the lyrics
     return(bm25.get_top_n(tokenized_query, All_doc, n=1))
+
+# print("bm25",bm25_index(All_doc,"They-are-rotting-my-brain"))
--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import json
 
-import jsonpickle as jsonpickle
+# import jsonpickle as jsonpickle
 from flask import Flask, render_template, send_file, request
 import os
 import pandas as pd
@@ -8,40 +8,65 @@ import re
 import math
 import numpy as np
 from rank_bm25 import BM25Okapi
-from NLPSearch import bm25_index
+from NLPSearch import*
 import sentiment_search as ss
 
 app = Flask(__name__)
 
-df = pd.read_csv("songs_sentiment.csv", encoding='unicode_escape', sep=",")
+df = pd.read_csv("songs.csv", encoding='unicode_escape',sep=",")
 ll = df['lyrics'].to_list()
 cleanedList = [x for x in ll if x != 'NaN']
-cleanedList
-ll[1]
-len(df.columns)
 # cleanedList
+
 
 df_ = df[df.lyrics != 'Error: Could not find lyrics.']
 df_ = df_[df_['lyrics'].notna()]
 ll = df_['lyrics'].to_list()
 
-# clean data to All_doc
+#clean data to All_doc
 All_doc = ll
 delimi = ",.!?/&-:;@'..."
 All_bagofwords = []
-for i in range(0, len(All_doc)):
-    re.split("[" + "\\".join(delimi) + "]", All_doc[i])
+for i in range(0,len(All_doc)):
+    re.split("["+"\\".join(delimi)+"]", All_doc[i])
     All_doc[i] = (' '.join(w for w in re.split(r"\W", All_doc[i]) if w)).lower()
     # print(All_doc[i].split())
     All_bagofwords.append(All_doc[i].split())
+    
 
-musicCorpus = ss.Music("songs_sentiment.csv")
-musicCorpus.load_data()
+@app.route('/bm25_index')
+def bm25():
+    keyword = request.args.get("keyword")
+    print(keyword)
+    
+    return bm25_index(All_doc, keyword),200
 
 
-@app.route('/I-m-a-little-unsteady-A-little-unsteady')
-def output():
-    return bm25_index(All_doc, "I-m-a-little-unsteady-A-little-unsteady")
+@app.route('/self_cosinesim')
+def self_cosinesim():
+    keyword = request.args.get("keyword")
+    MAP = fetchInMap(All_bagofwords)
+    allkey = mergeKey(MAP)
+    TF = []
+    for i in range(0,len(All_bagofwords)):
+        tf = computeTF(MAP[i],All_bagofwords[i])
+        TF.append(tf)
+    idf = computeIDF(MAP,allkey)
+    tfidf = []
+    for i in range(0, len(TF)):
+        tfidf.append(computeTFIDF(TF[i],idf))
+        
+    query = keyword
+    query = query.lower()
+    query_bow = query_BOW(query)
+    query_fr = query_num(query_bow,allkey)
+    query_tf = query_computeTF(query_fr,query_bow)
+    query_rank = cosine_similarity(query_tf,tfidf,query_tf)
+    
+    
+    import numpy as np
+    index = np.argsort(query_rank)[-5:]
+    return str(index[0]) ,200
 
 
 # use: 127.0.0.1：5000/sentiment_search？keyword=xxx
